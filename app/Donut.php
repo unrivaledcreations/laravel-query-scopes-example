f39f0e422e72375b94f319d918b7801182d42893
Michael Hall <mike@unrivaledcreations.com>
2017-08-03 02:09:59 -0400

    Global query scope only, class-based method

--- app/Donut.php
+++ app/Donut.php
@@ -3,23 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Carbon\Carbon;
 use App\Scopes\StaleScope;
 
 class Donut extends Model
 {
-    public function scopeExcludeFresh($query)
-    {
-        $now = Carbon::now();
-        return $query->where('baked_at', '<=', $now->subDay());
-    }
-
-    public function scopeExcludeStale($query)
-    {
-        $now = Carbon::now();
-        return $query->where('baked_at', '>=', $now->subDays(3));
-    }
-
     protected static function boot()
     {
         parent::boot();
