dd69d4952f4ea246fe637d97c3b77971ed30cb3c
Michael Hall <mike@unrivaledcreations.com>
2017-08-06 13:27:52 -0400

    More descriptive comments added to the Donut model class file.

--- app/Donut.php
+++ app/Donut.php
@@ -2,7 +2,13 @@
 
 namespace App;
 
+# php `artisan make:model Donut` automatically adds this `use` statement for you:
 use Illuminate\Database\Eloquent\Model;
+
+# But you must add this `use` statement in order for the `addGlobalScope(...)` closure to work:
+use Illuminate\Database\Eloquent\Builder;
+
+# NOT required by Laravel Eloquent global query scopes, but this example does need this:
 use Carbon\Carbon;
 
 class Donut extends Model
